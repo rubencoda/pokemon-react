b81be4de6b9975ce194dd896af64f02459d37b70
rubencoda2310 <rubencoda2310@gmail.com>
2024-01-08 15:05:05 +0700

    search pokemon

--- src/components/Datalist/PokemonList.jsx
+++ src/components/Datalist/PokemonList.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 const PokemonList = () => {
     const [pokemonList, setPokemonList] = useState([]);
     const [detailsArray, setDetailsArray] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
     
     useEffect(() => {
         // Fetching pokemon list
@@ -20,22 +21,22 @@ const PokemonList = () => {
     }, []);
 
     useEffect(() => {
-      const fetchPokemonDetails = async () => {
-        const detailsArray = await Promise.all(
-          pokemonList.map(async (pokemon) => {
-            const response = await axios.get(pokemon.url);
-            const details = response.data;
-            return details;
-          })
+      const filterPokemonList = async () => {
+        const filteredPokemonList = await Promise.all(
+          pokemonList
+            .filter((pokemon) => pokemon.name.includes(searchTerm.toLowerCase()))
+            .map(async (pokemon) => {
+              const response = await axios.get(pokemon.url);
+              const details = response.data;
+              return details;
+            })
         );
   
-        setDetailsArray(detailsArray);
+        setDetailsArray(filteredPokemonList);
       };
   
-      if (pokemonList.length > 0) {
-        fetchPokemonDetails();
-      }
-    }, [pokemonList]);
+      filterPokemonList();
+    }, [searchTerm, pokemonList]);
 
     console.log(detailsArray);
 
@@ -43,33 +44,32 @@ const PokemonList = () => {
       <div>
         <div className="container my-12 mx-auto">
           <div className="flex justify-center items-center">
-            <input type="text" className="px-4 py-2 border-2 w-full" placeholder="Search pokemon..."/>
-          </div>
+            <input type="text" className="px-4 py-2 border-2 w-full" onChange={(e) => setSearchTerm(e.target.value)} value={searchTerm} placeholder="Search pokemon..."/>
+          </div> 
           <div className="flex flex-wrap -mx-1 lg:-mx-4 justify-center mt-5"> 
-            {detailsArray.map((details, index) => (
+            {detailsArray.length > 0 ? detailsArray.map((details, index) => (
               <div key={index} className="my-1 px-1 max-w-xs md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3 ">
-              {/* Card */}
-              <div className="overflow-hidden rounded-lg shadow-lg">
-                {/* Pokemon Image */}
-                <img alt="Placeholder" className="block h-auto w-full" src={details.sprites.front_default} />
-                {/* Pokemon Name */}
-                <div className="bg-gold-pastel">
-                  <div className="flex items-center justify-between leading-tight p-2 md:p-4">
-                    <h1 className="font-bold text-xl">
-                      {details.name}
-                    </h1>
+                  <div className="overflow-hidden rounded-lg shadow-lg">
+                      <img alt="Placeholder" className="block h-auto w-full" src={details.sprites.front_default} />
+                      <div className="bg-gold-pastel">
+                        <div className="flex items-center justify-between leading-tight p-2 md:p-4">
+                          <h1 className="font-bold text-xl">
+                            {details.name}
+                          </h1>
+                        </div>
+                        <div className="flex items-center justify-between leading-none p-2 md:p-4">
+                          <div className="flex items-center no-underline  text-black">
+                            <p className="ml-2 text-sm">
+                              Abilities
+                            </p>
+                          </div>
+                        </div>
+                      </div>
                   </div>
-                  <div className="flex items-center justify-between leading-none p-2 md:p-4">
-                    <div className="flex items-center no-underline  text-black">
-                      <p className="ml-2 text-sm">
-                        Abilities
-                      </p>
-                    </div>
-                  </div>
-                </div>
               </div>
-            </div>
-            ))}
+            )) : (
+              <p>Data tot found</p>
+            )}
           </div>
         </div>
       </div>
